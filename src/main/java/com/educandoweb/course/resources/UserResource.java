f31e6d7cf6c2c82b62d4049f025db096eeaab5ca
Kayky Oliveira <kaykycf1245@gmail.com>
2023-12-14 16:27:20 -0300

    User delete

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +16,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
-
-import jakarta.servlet.Servlet;
 
 @RestController //Mostra que a classe é um recurso web, Implementado por um controlador Rest
 @RequestMapping(value = "/users") //Anotation que da o nome do recurso
@@ -42,6 +41,12 @@ public class UserResource {
 		obj = service.insert(obj);
 		URI uri	= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();				
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 
